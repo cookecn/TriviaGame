baa00f7afa7e9ce05bdf9cdcd3d4b2f07e06ab4a
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-02-28 11:58:50 -0600

    still some problems with jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 var startAtNumber = 60;
+var pageRefreshNumber = 10;
 
 var countDown;
+var pageReset;
 
 $("#start").on("click", start);
 $("#time").text(startAtNumber);
@@ -8,7 +10,6 @@ $("#submit").on("click", submit);
 
 
 function start() {
-    $("#start").attr("disabled", true);
     $("#allQuestions").css("display", "block");
     countDown = setInterval(decrement, 1000);
     console.log("#time");
@@ -18,13 +19,13 @@ function start() {
 function decrement() {
     startAtNumber--;
 
-    $("#time").append(startAtNumber);
+    $("#time").text(startAtNumber);
 
-    if (startAtNumber <= 0) {
+    if (startAtNumber === 0) {
         stop();
         alert("Time is Up! Click the start button again to re-try");
         $("#start").attr("disabled", false);
-    } 
+    }
 }
 
 function stop() {
@@ -33,15 +34,20 @@ function stop() {
 }
 
 function submit() {
-    $("answers").css("display", "block");
+    $("#answers").css("display", "block");
     stop();
+
+    if (startAtNumber !== 0) {
+        alert("If you wish to try again, click the start button at the top of the page!");
+        $("#start").attr("diabled", false);
+    }
     var trueAnswer = $("input:radio[value=true]:checked");
         trueAnswer = trueAnswer.length;
-        $("correctAnswers").html(trueAnswer);
+        $("#correctAnswers").html(trueAnswer);
 
     var falseAnswer = $("input:radio[value=false]:checked");
         falseAnswer = falseAnswer.length;
-        $("inCorrectAnswers").html(falseAnswer);
+        $("#inCorrectAnswers").html(falseAnswer);
 
     var notAnsweredCount = 0;
     for (var i = 1; i <= 10; i++) {
